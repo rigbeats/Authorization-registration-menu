42025b5db88acf533d0df9206d420bd2562769c1
rigbeats <rishat.gabdullin2016@mail.ru>
2023-07-27 20:29:26 +0600

    изменил форму

--- Life-tracker/Forms/Authorization.Designer.cs
+++ Life-tracker/Forms/Authorization.Designer.cs
@@ -90,7 +90,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(523, 379);
+            this.ClientSize = new System.Drawing.Size(1264, 681);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
