2d4b643ef85ef97b9788ed8830da4e5955a90296
rigbeats <rishat.gabdullin2016@mail.ru>
2023-07-28 16:39:44 +0600

    Создал клас для закругленной кнопки

---
+++ Life-tracker/RoundButton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Life_tracker
+{
+    internal class RoundButton
+    {
+        public static GraphicsPath RoundedRectangle(Rectangle rect, int RoundSize)
+        {
+            GraphicsPath gp = new GraphicsPath();
+
+            gp.AddArc(rect.X, rect.Y, RoundSize, RoundSize, 180, 90);
+
+            return gp;
+        }
+    }
+}
