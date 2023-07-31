d68feb1a360aeb827bfaf816774e9273b8b2c6a4
rigbeats <rishat.gabdullin2016@mail.ru>
2023-07-31 19:41:48 +0600

    добавил код для связи с БД

--- Life-tracker/Forms/Authorization.Designer.cs
+++ Life-tracker/Forms/Authorization.Designer.cs
@@ -79,6 +79,7 @@ namespace Life_tracker
             this.Name = "Authorization";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Авторизация";
+            this.Load += new System.EventHandler(this.Authorization_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
--- Life-tracker/Forms/Authorization.cs
+++ Life-tracker/Forms/Authorization.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Life_tracker.Entity_classes;
 
 namespace Life_tracker
 {
@@ -17,6 +18,21 @@ namespace Life_tracker
         public Authorization()
         {
             InitializeComponent();
+        }
+
+        private void Authorization_Load(object sender, EventArgs e)
+        {
+            using(var context = new UserDbContext())
+            {       
+                /*
+                var Users = context.users.ToList();
+
+                foreach (var user in Users)
+                {
+                    MessageBox.Show(user.Login);
+                }
+                */
+            }
         }
     }
 }
