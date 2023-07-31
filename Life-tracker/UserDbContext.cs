04e168b9b7334d77c820f98c0c969ebaa76741af
rigbeats <rishat.gabdullin2016@mail.ru>
2023-07-31 19:42:12 +0600

    Класс для связи с базой Users

---
+++ Life-tracker/UserDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Life_tracker.Entity_classes;
+
+namespace Life_tracker
+{
+    internal class UserDbContext : DbContext
+    {
+        public UserDbContext() : base("UserConnection")
+        {
+        }
+
+        public DbSet<User> users { get; set; }
+    }
+}
