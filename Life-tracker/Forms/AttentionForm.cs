5564b8bdad138d9164bd8c85bc83e69ae9a6b54e
rigbeats <rishat.gabdullin2016@mail.ru>
2023-08-08 20:28:07 +0600

    Добавил 2 новые формы для предупреждений

---
+++ Life-tracker/Forms/AttentionForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Life_tracker.Forms
+{
+    public partial class AttentionForm : Form
+    {
+        public AttentionForm(string text)
+        {
+            InitializeComponent();
+            WarningText.Text = text;
+        }
+
+        private void bunifuButton1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void AttentionForm_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Select();
+        }
+    }
+}
---
+++ Life-tracker/Forms/SuccessfulForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Life_tracker.Forms
+{
+    public partial class SuccessfulForm : Form
+    {
+        public SuccessfulForm(string text)
+        {
+            InitializeComponent();
+            tbWarningText.Text = text;
+        }
+
+        private void bunifuButton1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void SuccessfulForm_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Select();
+        }
+    }
+}
